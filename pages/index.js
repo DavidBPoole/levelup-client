2c46af45f7f3e4dc9233e9d6dccfaeb4d558ef2e
David Poole <dbpoole7@gmail.com>
2023-11-18 15:45:06 -0600

    added further functionality to games and events along with debugging many linter issues

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,40 @@
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
+import { useRouter } from 'next/router';
+import GameCard from '../components/game/GameCard';
+import { getGames } from '../api/gameData';
 
 function Home() {
-  const { user } = useAuth();
+  const [games, setGames] = useState([]);
+  const router = useRouter();
+
+  const getAllGames = () => {
+    getGames().then((data) => setGames(data));
+  };
+
+  useEffect(() => {
+    getAllGames();
+  }, []);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      <article className="games">
+        <h1>Games</h1>
+
+        <Button
+          onClick={() => {
+            router.push('/games/new');
+          }}
+        >
+          Register New Game
+        </Button>
+        {games.map((game) => (
+          <section key={`game--${game.id}`} className="game">
+            <GameCard id={game.id} title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} gameType={game.game_type} onUpdate={getAllGames} />
+          </section>
+        ))}
+      </article>
+    </>
   );
 }
 
